8be91face634fb05bad8f22fab34016312ef2468
Sam Samskies <samsamskies@gmail.com>
2024-08-02 12:44:35 -0500

    display QR on the note embed

--- components/Lnurl/Lnurl.tsx
+++ components/Lnurl/Lnurl.tsx
@@ -17,17 +17,30 @@ export const Lnurl = ({ id, error }: { id: string; error: string | null }) => {
     <main className={styles.main}>
       {!error && (
         <>
-          <iframe
-            src={`https://njump.me/${id}?embed=yes`}
-            className="nostr-embedded"
-            style={{
-              width: "100%",
-              height: 400,
-              border: "2px solid #C9C9C9",
-              borderRadius: 10,
-              marginBottom: 20,
-            }}
-          ></iframe>
+          <div className={styles.noteEmbedContainer}>
+            <iframe
+              src={`https://njump.me/${id}?embed=yes`}
+              className="nostr-embedded"
+              style={{
+                width: "100%",
+                height: 400,
+                border: "2px solid #C9C9C9",
+                borderRadius: 10,
+                marginBottom: 20,
+              }}
+            ></iframe>
+            <div className={styles.noteQrCodeContainer}>
+              <QRCodeSVG
+                value={`lightning:${lnurl}`}
+                includeMargin
+                size={100}
+                style={{
+                  width: 100,
+                  height: "auto",
+                }}
+              />
+            </div>
+          </div>
           <p className={styles.cta}>
             Scan this QR code with any lightning wallet to zap this Nostr event
             🎉
