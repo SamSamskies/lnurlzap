1eceeed836f420446ec33a54595b4ee1ce1ffcb8
Sam Samskies <samsamskies@gmail.com>
2024-08-11 17:16:44 -0500

    add lightning address support

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/lnurlp/:id',
+        destination: '/api/zap/:id',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
