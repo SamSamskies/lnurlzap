db1123ca23f3083dfbc2c0e4fc03f6beddc370b6
Sam Samskies <samsamskies@gmail.com>
2024-08-18 12:56:27 -0500

    remove last week's tunestr live stream alias

--- utils/aliases.ts
+++ utils/aliases.ts
@@ -5,9 +5,6 @@ const ALIASES: { [key: string]: string } = {
   notbiebs: "npub1vp8fdcyejd4pqjyrjk9sgz68vuhq7pyvnzk8j0ehlljvwgp8n6eqsrnpsw",
   thezapsmustflow:
     "note1uruklq5zazcelfgw4f4s9h7qkhapjxpawt9rm9vdw8ncv9tj00vq7s0tp0",
-  // TODO: remove after 8/12 live stream
-  tunestrlivestream:
-    "naddr1qq9rzdejxv6nqdpnxc6syg9e6qkt3lw7kxghq8kqvj8r0mgldta6yclqqc8uqcye5c59r5j7qspsgqqqwensd3kj2n",
 };
 
 export const getNip19Id = (alias: string) => ALIASES[alias];
