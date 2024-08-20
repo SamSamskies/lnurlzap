e7b4b1f901296ec978d277dd382c88818e719e16
Sam Samskies <samsamskies@gmail.com>
2024-08-19 21:31:59 -0500

    add temp tunestrlivestream alias

--- utils/aliases.ts
+++ utils/aliases.ts
@@ -5,6 +5,10 @@ const ALIASES: { [key: string]: string } = {
   notbiebs: "npub1vp8fdcyejd4pqjyrjk9sgz68vuhq7pyvnzk8j0ehlljvwgp8n6eqsrnpsw",
   thezapsmustflow:
     "note1uruklq5zazcelfgw4f4s9h7qkhapjxpawt9rm9vdw8ncv9tj00vq7s0tp0",
+
+  // TODO: remove this after the 8/19 stream ends
+  tunestrlivestream:
+    "naddr1qq9rzdejxscnqv3kxy6syg9e6qkt3lw7kxghq8kqvj8r0mgldta6yclqqc8uqcye5c59r5j7qspsgqqqwens6lyuea",
 };
 
 export const getNip19Id = (alias: string) => ALIASES[alias];
