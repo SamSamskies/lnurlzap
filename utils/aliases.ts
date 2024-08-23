bdf95085c52b6063698f6e4cfc5749f85ffb4c66
Sam Samskies <samsamskies@gmail.com>
2024-08-23 12:54:21 -0500

    add temp npc alias

--- utils/aliases.ts
+++ utils/aliases.ts
@@ -5,10 +5,7 @@ const ALIASES: { [key: string]: string } = {
   notbiebs: "npub1vp8fdcyejd4pqjyrjk9sgz68vuhq7pyvnzk8j0ehlljvwgp8n6eqsrnpsw",
   thezapsmustflow:
     "note1uruklq5zazcelfgw4f4s9h7qkhapjxpawt9rm9vdw8ncv9tj00vq7s0tp0",
-
-  // TODO: remove this after the 8/19 stream ends
-  tunestrlivestream:
-    "naddr1qq9rzdejxscnqv3kxy6syg9e6qkt3lw7kxghq8kqvj8r0mgldta6yclqqc8uqcye5c59r5j7qspsgqqqwens6lyuea",
+  npc: "naddr1qqjrgvphxycngcek95cxgefh956rxwfn95urwwty95mrgwf3x43kvvpkvvmngqg3waehxw309ahx7um5wgh8w6twv5hsygx0gknt5ymr44ldyyaq0rn3p5jpzkh8y8ymg773a06ytr4wldxz55psgqqqwensuy5xvs",
 };
 
 export const getNip19Id = (alias: string) => ALIASES[alias];
